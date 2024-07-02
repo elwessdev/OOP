08381e1db0f7d039573356e4395dfc040c13b201
devosema <benaliosama3@gmail.com>
2024-07-02 17:30:59 +0100

    first

---
+++ Abstraction.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using namespace std;
+
+int main_abstraction()
+{
+    cout << "abstraction" << endl;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using namespace std;
+
+int main()
+{
+    cout<<"OOP"<< endl;
+    return 0;
+}
